07ff202008ffe2d872e530b0ed60f1fd347e830f
Ryan Delos Santos <delossantosrryan@gmail.com>
2022-08-26 12:05:53 -0700

    implemented game game functions to js.script file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+let playerSelection = prompt(
+  'Choose your weapon: Rock 🪨, Paper 📄, Scissors ✂️'
+).toLowerCase();
+
+function getComputerChoice() {
+  const computerChoices = ['rock', 'paper', 'scissors'];
+
+  let computerSelection = Math.floor(Math.random() * computerChoices.length);
+  return computerChoices[computerSelection];
+}
+
+// getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  const playerWinsMsg = 'Player Wins';
+  const computerWinsMsg = 'Computer Wins';
+
+  console.log(`player: ${playerSelection} || computer: ${computerSelection}`);
+  if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    return playerWinsMsg;
+  } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+    return computerWinsMsg;
+  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    return playerWinsMsg;
+  } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    return computerWinsMsg;
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    return playerWinsMsg;
+  } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+    return computerWinsMsg;
+  } else {
+    return `TIE`;
+  }
+}
+
+console.log(playRound(playerSelection, getComputerChoice()));
